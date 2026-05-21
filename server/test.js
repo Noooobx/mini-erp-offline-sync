1638469200c2691ad581d31705b05ad083b504ac
SilentCodeSage <nandunandakishor345@gmail.com>
2026-05-21 17:01:28 +0530

    Chore: Removed debug scripts

--- server/test.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const p = require('./src/db/db');
-p.query("SELECT column_name, is_nullable FROM information_schema.columns WHERE table_name = 'sales';").then(r => { console.log(r.rows); process.exit(0); })
